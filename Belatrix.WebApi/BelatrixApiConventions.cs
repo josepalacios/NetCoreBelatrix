a10b07247ed23948fb9c0b79678854f1352e94a2
Jose Palacios <jose.palaciosf@gmail.com>
2019-06-17 17:07:48 -0500

    Add  swagger

---
+++ Belatrix.WebApi/BelatrixApiConventions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Belatrix.WebApi
+{
+    public static  class BelatrixApiConventions
+    {
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Get([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)] [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)] object id)
+        { }
+
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Get()
+        { }
+
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Post([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)] [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)] object id)
+        { }
+
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Put([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)] [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)] object id)
+        { }
+
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public static void Delete([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)] [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)] object id)
+        { }
+    }
+}
